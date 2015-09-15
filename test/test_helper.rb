421371ee462576813560927def5eb7b7079790cf
Josh Cheek <josh.cheek@gmail.com>
2015-09-15 17:40:48 -0600

    An acceptance test

---
+++ test/test_helper.rb
@@ -0,0 +1,18 @@
+lib_dir = File.expand_path '../../headcount/lib', __dir__
+$LOAD_PATH.unshift lib_dir
+require_relative File.expand_path('headcount', lib_dir)
+
+
+class TestHarness < Minitest::Test
+  def self.data_dir
+    File.expand_path '../data', __dir__
+  end
+
+  def self.repo
+    @repo ||= DistrictRepository.from_csv(data_dir)
+  end
+
+  def repo
+    self.class.repo
+  end
+end
---
+++ test/test_loading_districts.rb
@@ -0,0 +1,70 @@
+require_relative 'test_helper'
+
+# e = results.select { |f|
+#   f.first[:file] == 'Pupil enrollment.csv'
+# }.first
+#
+# [10] pry(DistrictRepository)> e.select { |r| r[:location] == 'ACADEMY 20' }
+# => [{:location=>"ACADEMY 20", :timeframe=>"2009", :dataformat=>"Number", :data=>"22620", :file=>"Pupil enrollment.csv"},
+#     {:location=>"ACADEMY 20", :timeframe=>"2010", :dataformat=>"Number", :data=>"23119", :file=>"Pupil enrollment.csv"},
+#     {:location=>"ACADEMY 20", :timeframe=>"2011", :dataformat=>"Number", :data=>"23657", :file=>"Pupil enrollment.csv"},
+#     {:location=>"ACADEMY 20", :timeframe=>"2012", :dataformat=>"Number", :data=>"23973", :file=>"Pupil enrollment.csv"},
+#     {:location=>"ACADEMY 20", :timeframe=>"2013", :dataformat=>"Number", :data=>"24481", :file=>"Pupil enrollment.csv"},
+#     {:location=>"ACADEMY 20", :timeframe=>"2014", :dataformat=>"Number", :data=>"24578", :file=>"Pupil enrollment.csv"}]
+
+
+class TestLoadingDistricts < TestHarness
+  def test_knows_all_the_enrollment_data
+    district = repo.find_by_name("ACADEMY 20")
+    assert_equal 22620, district.enrollment.in_year(2009)
+    assert_equal 0.895, district.enrollment.graduation_rate.for_high_school_in_year(2010)
+    assert_equal 0.857, district.statewide_testing.proficient_for_subject_by_grade_in_year(:math, 3, 2008)
+  end
+end
+
+
+__END__
+
+EconomicProfile
+  Median household income.csv
+  School-aged children in poverty.csv
+  Students qualifying for free or reduced price lunch.csv
+  Title I students.csv
+
+StatewideTesting
+  3rd grade students scoring proficient or above on the CSAP_TCAP.csv
+  4th grade students scoring proficient or above on the CSAP_TCAP.csv
+  8th grade students scoring proficient or above on the CSAP_TCAP.csv
+  Average proficiency on the CSAP_TCAP by race_ethnicity_Math.csv
+  Average proficiency on the CSAP_TCAP by race_ethnicity_Reading.csv
+  Average proficiency on the CSAP_TCAP by race_ethnicity_Writing.csv
+
+Enrollment
+  Dropout rates by race and ethnicity.csv
+  High school graduation rates.csv
+  Kindergartners in full-day program.csv
+  Online pupil enrollment.csv
+  Pupil enrollment by race_ethnicity.csv
+  Pupil enrollment.csv
+  Remediation in higher education.csv
+  Special education.csv
+
+
+"data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv" Location,Score,TimeFrame,DataFormat,Data
+"data/4th grade students scoring proficient or advanced on CSAP_TCAP.csv"  Location,TimeFrame,DataFormat,Data
+"data/8th grade students scoring proficient or above on the CSAP_TCAP.csv" Location,Score,TimeFrame,DataFormat,Data
+"data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv"    Location,Race Ethnicity,TimeFrame,DataFormat,Data
+"data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv" Location,Race Ethnicity,TimeFrame,DataFormat,Data
+"data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv" Location,Race Ethnicity,TimeFrame,DataFormat,Data
+"data/Dropout rates by race and ethnicity.csv"                             Location,Category,TimeFrame,DataFormat,Data
+"data/High school graduation rates.csv"                                    Location,TimeFrame,DataFormat,Data
+"data/Kindergartners in full-day program.csv"                              Location,TimeFrame,DataFormat,Data
+"data/Median household income.csv"                                         Location,TimeFrame,DataFormat,Data
+"data/Online pupil enrollment.csv"                                         Location,TimeFrame,DataFormat,Data
+"data/Pupil enrollment by race_ethnicity.csv"                              Location,Race,TimeFrame,DataFormat,Data
+"data/Pupil enrollment.csv"                                                Location,TimeFrame,DataFormat,Data
+"data/Remediation in higher education.csv"                                 Location,TimeFrame,DataFormat,Data
+"data/School-aged children in poverty.csv"                                 Location,TimeFrame,DataFormat,Data
+"data/Special education.csv"                                               Location,TimeFrame,DataFormat,Data
+"data/Students qualifying for free or reduced price lunch.csv"             Location,Poverty Level,TimeFrame,DataFormat,Data
+"data/Title I students.csv"                                                Location,TimeFrame,DataFormat,Data
