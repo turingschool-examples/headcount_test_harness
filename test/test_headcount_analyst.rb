a46dee51d911874b626e1ca5fe436cbd27915100
Ross Edfort <rossedfort@gmail.com>
2015-09-22 16:52:56 -0600

    took out a fckn pry

--- test/test_headcount_analyst.rb
+++ test/test_headcount_analyst.rb
@@ -6,4 +6,10 @@ class TestHeadcountAnalyst < TestHarness
     result = ha.top_statewide_testing_year_over_year_growth_in_3rd_grade(:math)
     assert_equal result, ["WILEY RE-13 JT", 0.300]
   end
+
+  def test_kindergarten_participation_rate_variation_returns_a_floating_point_value_truncated_to_three_decimels
+    ha = HeadcountAnalyst.new(repo)
+
+    assert_equal -0.123, ha.kindergarten_participation_rate_variation("ACADEMY 20")
+  end
 end
