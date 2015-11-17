c5b049784e57a90b2b94ad328c3a7787def58a04
Beth Secor <elizabeth.a.secor@gmail.com>
2015-11-17 08:51:09 -0700

    Fixed typos on line 17 and 47

--- test/iteration_two_test.rb
+++ test/iteration_two_test.rb
@@ -30,7 +30,7 @@ class IterationTwoTest < Minitest::Test
   def test_basic_proficiency_by_race
     str = statewide_repo
     testing = str.find_by_name("ACADEMY 20")
-    asian = { 2011 => {math: 0.816, reading: 0.897, writing: 0.826},
+    expected = { 2011 => {math: 0.816, reading: 0.897, writing: 0.826},
               2012 => {math: 0.818, reading: 0.893, writing: 0.808},
               2013 => {math: 0.805, reading: 0.901, writing: 0.810},
               2014 => {math: 0.800, reading: 0.855, writing: 0.789},
@@ -44,7 +44,7 @@ class IterationTwoTest < Minitest::Test
     str.load_data({
                     :statewide_testing => {
                       :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
-                      :eigth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
+                      :eighth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
                       :math => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv",
                       :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
                       :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"
