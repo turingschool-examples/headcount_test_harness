77353445a037a90fec02a8f5990d179604edf8df
Horace Williams <horace.d.williams@gmail.com>
2015-11-16 19:37:49 -0700

    wip starting iteration 2 specs

---
+++ test/iteration_two_test.rb
@@ -0,0 +1,55 @@
+require "minitest"
+require "minitest/autorun"
+require_relative "../../headcount/lib/district_repository"
+require_relative "../../headcount/lib/district"
+require_relative "../../headcount/lib/enrollment"
+require_relative "../../headcount/lib/headcount_analyst"
+
+class IterationTwoTest < Minitest::Test
+  def test_statewide_testing_repository_basics
+    str = statewide_repo
+    assert str.find_by_name("ACADEMY 20")
+    assert str.find_by_name("GUNNISON WATERSHED RE1J")
+  end
+
+  def test_basic_proficiency_by_grade
+    str = statewide_repo
+    expected = { 2008 => {:math => 0.857, :reading => 0.866, :writing => 0.671},
+                 2009 => {:math => 0.824, :reading => 0.862, :writing => 0.706},
+                 2010 => {:math => 0.849, :reading => 0.864, :writing => 0.662},
+                 2011 => {:math => 0.819, :reading => 0.867, :writing => 0.678},
+                 2012 => {:math => 0.830, :reading => 0.870, :writing => 0.655},
+                 2013 => {:math => 0.855, :reading => 0.859, :writing => 0.668},
+                 2014 => {:math => 0.834, :reading => 0.831, :writing => 0.639}
+               }
+
+    testing = str.find_by_name("ACADEMY 20")
+    assert_equal expected, testing.proficient_by_grade(3)
+  end
+
+  def test_basic_proficiency_by_race
+    str = statewide_repo
+    testing = str.find_by_name("ACADEMY 20")
+    asian = { 2011 => {math: 0.816, reading: 0.897, writing: 0.826},
+              2012 => {math: 0.818, reading: 0.893, writing: 0.808},
+              2013 => {math: 0.805, reading: 0.901, writing: 0.810},
+              2014 => {math: 0.800, reading: 0.855, writing: 0.789},
+            }
+    assert_equal expected, testing.proficient_by_race_or_ethnicity(:asian)
+  end
+
+
+  def statewide_repo
+    str = StatewideTestRepository.new
+    str.load_data({
+                    :statewide_testing => {
+                      :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
+                      :eigth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
+                      :math => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv",
+                      :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
+                      :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"
+                    }
+                  })
+    str
+  end
+end
